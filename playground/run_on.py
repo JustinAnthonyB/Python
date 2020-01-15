e1bb5533bee899ef5926f21228326a5c45b052e7
Justin <justinanthony.baptiste@georgebrown.ca>
2020-01-14 19:59:30 -0500

    Add files via upload

--- playground/run_on.py
+++ playground/run_on.py
@@ -1,29 +1,15 @@
-def listToString(someList):
-    for i in range(len(someList)):
-        newString = str(someList[i])
+import sys
 
-        if i == (len(someList)-1):
-            print('and ' + str(someList[i]))
-        else:
-            print(newString, end=', ')
+def comma(items):
+    for i in range(len(items) -2):
+        print(items[i] + ', ', end="")
+    print(items[-2] + ' and ' + items[-1]) 
 
 
+answer = input("Give me some items for a list (no spaces) or type exit to quit ")
+if answer == "exit":
+    sys.exit()
+else:
+    comma(answer)
 
-arg = [x for x in input().split()]
-listToString(arg)
 
-# listToString = []
-# while True:
-#     print('Enter the name of cat ' + str(len(listToString) + 1) +
-#       ' (Or enter nothing to stop.):')
-#     someList = input()
-#     if someList == '':
-#         break
-#     listToString = listToString + [someList] # list concatenation
-
-# listToString(someList)
-
-    
-# print('The cat names are:')
-# for name in catNames:
-#     print('  ' + name)
