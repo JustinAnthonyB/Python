8264ff6f1e6af1f533ce7d49f6863576cb297a48
Justin <justinanthony.baptiste@georgebrown.ca>
2020-03-04 11:54:51 -0500

    Add files via upload

--- files_ex.py
+++ files_ex.py
@@ -8,27 +8,22 @@ c.	A function to add the items in the following list to the file: female_oscar_w
 d.	Re-write the above functions as new functions with an "_ex" (for exceptions) added at the end of their name. The new functions code must handle multiple exceptions as needed. 
 '''
 
-
+def start():
+    global x
+    x=""
+    print("Enter file name: (most likely students.txt")
+    x = input(str(x))
+    
 
 def r_line():
-    x=""
-    print("Pick a file to read line by line: ")
-    x = input(str(x))
+    print("a.	A function to read each line from the file and print its content line by line. The function should work if you want to print lines from a different text file too.")
     with open(x) as file:
         for line in file:
             print(line, end="") 
             print()
 
 def r_doc():
-    x=""
-    print("Pick a file to read into a list: ")
-    x = input(str(x))
-    # with open(x) as file: 
-    #     x = file.readlines() 
-    #     print(x[0], end="") 
-    #     print(x[1])
-    #     print(x[:])
-    #     print(x)
+    print("b.	A function that should read the entire file content as a list.")
     with open(x) as file:
         lines = file.read().splitlines()
         print(type(lines))
@@ -37,24 +32,19 @@ def r_doc():
 
 
 def add_item():
-    x=""
-    print("Pick a file to add a list to: ")
-    x = input(x)
-    # members = ["John Cleese", "Eric Idle"]
+    print("A function to add a list to the file")
     female_oscar_win = [["Hillary Swank", 1999],["Julia Roberts", 2000],["Halle Berry", 2001], ["Nicole Kidman", 2002],["Charlize Theron", 2003]] 
-    with open(x, "a") as file: 
+    with open(x, "w") as file: 
         for m in female_oscar_win:
             file.write(str(m) + "\n")
+    print("Here is how the file ended up: ")
     with open(x) as file:
         for line in file:
             print(line, end="") 
             print()
 
-
-# def new_thing() << this is number d.
-
-
 def main():
+    start()
     r_line()
     r_doc()
     add_item()
